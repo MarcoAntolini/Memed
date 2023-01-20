cef6ad8de1a9ca271ec8f4146b42db6621847fa3
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-20 18:00:45 +0100

    fix variable scope

--- memed/db/database.php
+++ memed/db/database.php
@@ -5,7 +5,7 @@ class DatabaseHelper{
     public function __construct($servername, $username, $password, $dbname, $port){
         $this->db = new mysqli($servername, $username, $password, $dbname, $port);
         if ($this->db->connect_error) {
-            die("Connection failed: " . $db->connect_error);
+            die("Connection failed: " . $this->db->connect_error);
         }        
     }
 
@@ -227,4 +227,3 @@ class DatabaseHelper{
         }
     }
 }
-?>
