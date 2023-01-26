d1709b27042b47c506208243ac7347a4d53a5a29
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-01-26 23:29:39 +0100

    correzione file login

--- memed/login.php
+++ memed/login.php
@@ -1,17 +1,21 @@
 <?php
+include 'functions.php';
 require_once 'bootstrap.php';
 
-if (isset($_POST["username"]) && isset($_POST["password"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $user = $dbh->getUserLogin($username, $password);
-    if ($user) {
-        registerLoggedUser($user["IDuser"]);
-        header("Location: index.php");
+if(isset($_POST['email'], $_POST['p'])) { 
+    $email = $_POST['email'];
+    $password = $_POST['p']; // Recupero la password criptata.
+    if(login($email, $password, $mysqli) == true) {
+       // Login eseguito
+       echo 'Success: You have been logged in!';
     } else {
-        echo "Username or password is incorrect";
+       // Login fallito
+       header('Location: ./login.php?error=1');
     }
-}
+ } else { 
+    // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
+    echo 'Invalid Request';
+ }
 
 $templateParams["titolo"] = "Memed";
 $templeteParams["nome"]=""; //file da usare 
