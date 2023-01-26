e4de1fb78b234381b8b525c97c9255d868f9e990
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-01-26 23:21:44 +0100

    creato un login check

--- memed/utils/function.php
+++ memed/utils/function.php
@@ -45,4 +45,40 @@ function login($email, $password, $mysqli) {
     }
  }
 
+ function login_check($mysqli) {
+    // Verifica che tutte le variabili di sessione siano impostate correttamente
+    if(isset($_SESSION['username'], $_SESSION['login_string'])) {
+      $login_string = $_SESSION['login_string'];
+      $username = $_SESSION['username'];     
+      $user_browser = $_SERVER['HTTP_USER_AGENT']; // reperisce la stringa 'user-agent' dell'utente.
+      if ($stmt = $mysqli->prepare("SELECT password FROM utenti WHERE username = ? LIMIT 1")) { 
+         $stmt->bind_param('i', $username); // esegue il bind del parametro '$username'.
+         $stmt->execute(); // Esegue la query creata.
+         $stmt->store_result();
+  
+         if($stmt->num_rows == 1) { // se l'utente esiste
+            $stmt->bind_result($password); // recupera le variabili dal risultato ottenuto.
+            $stmt->fetch();
+            $login_check = hash('sha512', $password.$user_browser);
+            if($login_check == $login_string) {
+               // Login eseguito!!!!
+               return true;
+            } else {
+               //  Login non eseguito
+               return false;
+            }
+         } else {
+             // Login non eseguito
+             return false;
+         }
+      } else {
+         // Login non eseguito
+         return false;
+      }
+    } else {
+      // Login non eseguito
+      return false;
+    }
+ }
+
 ?>
