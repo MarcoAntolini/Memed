7f7e8b6b3c84265c856d2cb776f8ca8259395528
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-01-26 23:50:50 +0100

    modifica alla registrazione, nuovo file con funzioni per crittografia su js

--- memed/registration.php
+++ memed/registration.php
@@ -1,17 +1,18 @@
 <?php
 require_once 'bootstrap.php';
 
-if(isset($_POST["username"]) && isset($_POST["emal"]) && isset($_POST["password"]) && isset($_POST["confermapassword"])){
-    if(!$dbh->getUserByusername($_POST["username"]) && $_POST["password"]==$_POST["confermapassword"]){
-        $dbh->inserisciUtente($_POST["username"], $_POST["emal"], $_POST["password"]);
-        $user = $dbh->getUserLogin($_POST["username"], $_POST["password"]);
-        registerLoggedUser($user["username"]);
-        header("Location: index.php");
-    }else{
-        $templateParams["erroreregistrazione"] = "Username already exists";
-    }
-
-
+// Recupero la password criptata dal form di inserimento.
+$password = $_POST['p']; 
+// Crea una chiave casuale
+$random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+// Crea una password usando la chiave appena creata.
+$password = hash('sha512', $password.$random_salt);
+// Inserisci a questo punto il codice SQL per eseguire la INSERT nel tuo database
+// Assicurati di usare statement SQL 'prepared'.
+if ($insert_stmt = $mysqli->prepare("INSERT INTO utenti (username, email, password, salt) VALUES (?, ?, ?, ?)")) {    
+   $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt); 
+   // Esegui la query ottenuta.
+   $insert_stmt->execute();
 }
 
 $templateParams["titolo"] = "Memed registration";
