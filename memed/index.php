6df527fc820fe4d6536aedd81b1b7166099a47c1
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-01-26 22:48:46 +0100

    correzione funzioni

--- memed/index.php
+++ memed/index.php
@@ -3,9 +3,9 @@ require_once 'bootstrap.php';
 
 $templeteParams["titilo"]="Memed";
 $templeteParams["nome"]=""; //file da usare 
-$templeteParams["username"]=$dbh->getUser($_SESSION["IDuser"])["username"];
-$templeteParams["posthome"]=$dbh->getPostForHome($_SESSION["IDuser"]);
-$templeteParams["numNotifiche"]=$dbh->countNotifiche($_SESSION["IDuser"]);
+$templeteParams["username"]=$_SESSION["username"];
+$templeteParams["posthome"]=$dbh->getPostForHome($_SESSION["username"]);
+$templeteParams["numNotifiche"]=$dbh->countNotifiche($_SESSION["username"]);
 
 require 'template/base.php';
 ?>
--- memed/registration.php
+++ memed/registration.php
@@ -3,9 +3,9 @@ require_once 'bootstrap.php';
 
 if(isset($_POST["username"]) && isset($_POST["emal"]) && isset($_POST["password"]) && isset($_POST["confermapassword"])){
     if(!$dbh->getUserByusername($_POST["username"]) && $_POST["password"]==$_POST["confermapassword"]){
-        $dbh->inserisciUtente($dbh->getlastiduser(), $_POST["username"], $_POST["emal"], $_POST["password"]);
+        $dbh->inserisciUtente($_POST["username"], $_POST["emal"], $_POST["password"]);
         $user = $dbh->getUserLogin($_POST["username"], $_POST["password"]);
-        registerLoggedUser($user["IDuser"]);
+        registerLoggedUser($user["username"]);
         header("Location: index.php");
     }else{
         $templateParams["erroreregistrazione"] = "Username already exists";
