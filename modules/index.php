c263da5362d540434f78836cff8448d2e1356a9b
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-01-27 15:46:56 +0100

    Update index.php

--- modules/index.php
+++ modules/index.php
@@ -4,8 +4,8 @@ require_once 'bootstrap.php';
 $templeteParams["titilo"]="Memed";
 $templeteParams["nome"]=""; //file da usare 
 $templeteParams["username"]=$_SESSION["username"];
-$templeteParams["posthome"]=$dbh->getPostForHome($_SESSION["username"]);
-$templeteParams["numNotifiche"]=$dbh->countNotifiche($_SESSION["username"]);
+$templeteParams["posthome"]=$mysqli->getPostForHome($_SESSION["username"]);
+$templeteParams["numNotifiche"]=$mysqli->countNotifiche($_SESSION["username"]);
 
 require 'template/base.php';
 ?>
