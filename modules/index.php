0d46f4427bf6ee2f6a340f3e2da85f3285794ab1
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-01-27 15:48:04 +0100

    Update index.php

--- modules/index.php
+++ modules/index.php
@@ -1,11 +1,15 @@
 <?php
 require_once 'bootstrap.php';
 
-$templeteParams["titilo"]="Memed";
-$templeteParams["nome"]=""; //file da usare 
-$templeteParams["username"]=$_SESSION["username"];
-$templeteParams["posthome"]=$mysqli->getPostForHome($_SESSION["username"]);
-$templeteParams["numNotifiche"]=$mysqli->countNotifiche($_SESSION["username"]);
+if(login_check($mysqli) == true) {
+    $templeteParams["titilo"]="Memed";
+    $templeteParams["nome"]=""; //file da usare 
+    $templeteParams["username"]=$_SESSION["username"];
+    $templeteParams["posthome"]=$mysqli->getPostForHome($_SESSION["username"]);
+    $templeteParams["numNotifiche"]=$mysqli->countNotifiche($_SESSION["username"]);
+ } else {
+    echo 'You are not authorized to access this page, please login. <br/>';
+ }
 
 require 'template/base.php';
 ?>
