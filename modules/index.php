31b39b5d17d32d5b5225a9f866fd4fb46d4756f8
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-30 17:02:00 +0100

    insert right require statement

--- modules/index.php
+++ modules/index.php
@@ -1,15 +1,15 @@
 <?php
-require_once 'bootstrap.php';
+require_once "bootstrap.php";
 
-if(login_check($mysqli) == true) {
-    $templeteParams["titilo"]="Memed";
-    $templeteParams["nome"]=""; //file da usare 
-    $templeteParams["username"]=$_SESSION["username"];
-    $templeteParams["posthome"]=$mysqli->getPostForHome($_SESSION["username"]);
-    $templeteParams["numNotifiche"]=$mysqli->countNotifiche($_SESSION["username"]);
- } else {
-    echo 'You are not authorized to access this page, please login. <br/>';
- }
+// if(login_check($mysqli) == true) {
+$templeteParams["titolo"] = "Memed";
+$templeteParams["nome"] = ""; //file da usare 
+$templeteParams["username"] = $_SESSION["username"];
+$templeteParams["posthome"] = $mysqli->getPostForHome($_SESSION["username"]);
+$templeteParams["numNotifiche"] = $mysqli->countNotifiche($_SESSION["username"]);
+//  } else {
+//     echo 'You are not authorized to access this page, please login. <br/>';
+//     require "../template/login.php";
+//  }
 
-require 'template/base.php';
-?>
+require "../template/home.php";
