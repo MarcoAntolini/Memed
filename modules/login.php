2ba8fe3aa763933cf9799059209fc57a99e3f396
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-01 17:47:18 +0100

    Update login.php

--- modules/login.php
+++ modules/login.php
@@ -1,15 +1,15 @@
 <?php
 require_once 'bootstrap.php';
 
-if(isset($_POST['email'], $_POST['p'])) { 
+if(isset($_POST['email'], $_POST['password'])) { 
     $email = $_POST['email'];
-    $password = $_POST['p']; // Recupero la password criptata.
+    $password = $_POST['password']; // Recupero la password criptata.
     if(login($email, $password, $mysqli) == true) {
        // Login eseguito
-       echo 'Success: You have been logged in!';
+      header('Location: ../index.php');
     } else {
        // Login fallito
-       header('Location: ./login.php?error=1');
+       $templateParams['errorerelogin'] = 'email o password errati';
     }
  } else { 
     // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
@@ -18,3 +18,7 @@ if(isset($_POST['email'], $_POST['p'])) {
 
 $templateParams["titolo"] = "Login | Memed";
 $templateParams["nome"]="../template/login.php";
+
+require "../template/......php"; //file da usare
+//TODO
+?>
