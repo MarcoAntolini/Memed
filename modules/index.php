f47e1aa99bf86eb11ea0bd6ae08a4b022596561f
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-01 16:02:31 +0100

    fix index.php to start the website
    Co-Authored-By: Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
    Co-Authored-By: DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>

--- modules/index.php
+++ modules/index.php
@@ -4,10 +4,12 @@ require_once "bootstrap.php";
 // if(login_check($mysqli) == true) {
 $templateParams["titolo"] = "Memed";
 $templateParams["nome"] = "../template/home.php"; //file da usare 
-$templateParams["username"] = $_SESSION["username"];
-$templateParams["posthome"] = $mysqli->ottieniPostPerHome($_SESSION["username"]);
-$templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
+// $templateParams["username"] = $_SESSION["username"];
+// $templateParams["posthome"] = $mysqli->ottieniPostPerHome($_SESSION["username"]);
+// $templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
 //  } else {
 //     echo 'You are not authorized to access this page, please login. <br/>';
 //     require "../template/login.php";
 //  }
+
+require "../template/base.php";
