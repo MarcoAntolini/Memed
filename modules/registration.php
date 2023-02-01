56783c394685d10bab19196b40101f1c3c7d38ae
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-01 17:01:54 +0100

    Update registration.php

--- modules/registration.php
+++ modules/registration.php
@@ -2,7 +2,18 @@
 require_once 'bootstrap.php';
 
 // Recupero la password criptata dal form di inserimento.
-$password = $_POST['p'];
+if($mysqli->ottieniUtente($_POST['username'])){
+      $error_msg .= '<p class="error">Username già esistente</p>';
+}
+if($mysqli->ottieniUtenteemail($_POST['email'])){
+      $error_msg .= '<p class="error">Email già esistente</p>';
+}
+if($_POST['password'] != $_POST['confirm-password']){
+      $error_msg .= '<p class="error">Le password non coincidono</p>';
+}
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 // Crea una chiave casuale
 $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
 // Crea una password usando la chiave appena creata.
@@ -17,3 +28,6 @@ if ($insert_stmt = $mysqli->getMysqli()->prepare("INSERT INTO utenti (username, 
 
 $templateParams["titolo"] = "Memed registration";
 $templateParams["nome"] = "../template/register.php";
+
+require "../template/......php"; //file da usare
+?>
