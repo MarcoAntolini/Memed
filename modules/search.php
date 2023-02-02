3b5cd58c82ad9de03a6d749c342c9388d7a0f700
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-02 17:04:25 +0100

    Create search.php

---
+++ modules/search.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'bootstrap.php';
+
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $result = $mysqli("SELECT username FROM users WHERE username LIKE '%$username%'");
+
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['username'] . "\n";
+        }
+    } else {
+        echo "Nessun risultato.";
+    }
+}
+
+require "../template/base.php";
