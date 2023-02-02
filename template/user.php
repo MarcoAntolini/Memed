02a1fde38ecb439b76868629e4d6880af2ec8ba5
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 17:57:30 +0100

    Create user.php
    Co-Authored-By: DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>

---
+++ template/user.php
@@ -0,0 +1,74 @@
+<div class="my-profile">
+    <div class="container">
+        <div class="user-data-section">
+            <img class="profile-pic" src="<?php echo $templateParams["profilo"][0]["nomefile"]; ?>" alt="profile-pic">
+            <div class="username">
+                <?php echo $templateParams["profilo"][0]["username"]; ?>
+            </div>
+            <div class="average-reaction">
+                <!-- TODO: get average reaction -->
+            </div>
+            <div class="bio">
+                <?php echo $templateParams["profilo"][0]["bio"]; ?>
+            </div>
+            <button class="follower-count btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target=".follower-list">
+                Follower: <?php echo $templateParams["nFol"][0]; ?>
+            </button>
+            <button class="followed-count btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target=".followed-list">
+                Seguiti: <?php echo $templateParams["nSeguiti"][0]; ?>
+            </button>
+            <div class="follower-list modal fade" role="dialog" data-bs-keyboard="false" data-bs-backdrop="static" tabindex="-1">
+                <div id="follower-list" class="modal-dialog modal-dialog-scrollable modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Follower</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php foreach ($templateParams["follower"] as $follower) : ?>
+                            <div class="follower">
+                                <a href="user.php?username=<?php echo $follower["username"]; ?>">
+                                    <?php echo $follower["username"]; ?>
+                                </a>
+                            </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="followed-list modal fade" role="dialog" data-bs-keyboard="false" data-bs-backdrop="static" tabindex="-1">
+                <div id="followed-list" class="modal-dialog modal-dialog-scrollable modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Seguiti</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <?php if ($templateParams["seguiti"]) : ?>
+                            <?php foreach ($templateParams["seguiti"] as $followed) : ?>
+                            <div class="followed">
+                                <a href="user.php?username=<?php echo $followed["username"]; ?>">
+                                    <?php echo $followed["username"]; ?>
+                                </a>
+                            </div>
+                            <?php endforeach; ?>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="posts-section container">
+            <?php
+            if (isset($templateParams["js"])) :
+                echo '<div id="post-section"></div>';
+                foreach ($templateParams["js"] as $script) :
+            ?>
+            <script src="<?php echo $script; ?>"></script>
+            <?php
+                endforeach;
+            endif;
+            ?>
+        </div>
+    </div>
+</div>
