79c77f413bc5cd403ba915b7fbbeee7e2715dd50
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 10:53:31 +0100

    format code

--- modules/registration.php
+++ modules/registration.php
@@ -1,28 +1,30 @@
 <?php
 require_once 'bootstrap.php';
 
-// Recupero la password criptata dal form di inserimento.
-if ($mysqli->ottieniUtente($_POST['username'])) {
-   $templateParams['erroreregistrazione'] = 'Username già esistente';
+if (isset($_POST['username'], $_POST['email'], $_POST['password'], $_POST['confirm-password'])) {
+   // Recupero la password criptata dal form di inserimento.
+   if ($mysqli->ottieniUtente($_POST['username'])) {
+      $templateParams['erroreregistrazione'] = 'Username già esistente';
+   }
+   if ($mysqli->ottieniEmailDaUtente($_POST['email'])) {
+      $templateParams['erroreregistrazione'] = 'Email già esistente';
+   }
+   if ($_POST['password'] != $_POST['confirm-password']) {
+      $templateParams['erroreregistrazione'] = 'Le password non coincidono';
+   }
+   $username = $_POST['username'];
+   $email = $_POST['email'];
+   $password = $_POST['password'];
+   // Crea una chiave casuale
+   $random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
+   // Crea una password usando la chiave appena creata.
+   $password = hash('sha512', $password . $random_salt);
+   // Inserisci a questo punto il codice SQL per eseguire la INSERT nel tuo database
+   // Assicurati di usare statement SQL 'prepared'.
+   $mysqli->inserisciUtente($username, $email, $password, $salt);
 }
-if ($mysqli->ottieniEmailDaUtente($_POST['email'])) {
-   $templateParams['erroreregistrazione'] = 'Email già esistente';
-}
-if ($_POST['password'] != $_POST['confirm-password']) {
-   $templateParams['erroreregistrazione'] = 'Le password non coincidono';
-}
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-// Crea una chiave casuale
-$random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
-// Crea una password usando la chiave appena creata.
-$password = hash('sha512', $password . $random_salt);
-// Inserisci a questo punto il codice SQL per eseguire la INSERT nel tuo database
-// Assicurati di usare statement SQL 'prepared'.
-$mysqli->inserisciUtente($username, $email, $password, $salt);
 
 $templateParams["titolo"] = "Registrati | Memed";
-$templateParams["nome"] = "../template/register.php";
+$templateParams["nome"] = "../template/registration.php";
 
 require "../template/baseUnlogged.php";
