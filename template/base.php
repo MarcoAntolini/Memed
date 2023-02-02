33aed1427d6ddb4c26cfe9fac8d2edf68b223e66
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 17:57:53 +0100

    delete unused function
    Co-Authored-By: DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>

--- template/base.php
+++ template/base.php
@@ -20,16 +20,6 @@
         require $templateParams["nome"];
     }
     ?>
-    <?php
-    if (isset($templateParams["js"])) :
-        echo '<div id="post-section"></div>';
-        foreach ($templateParams["js"] as $script) :
-    ?>
-    <script src="<?php echo $script; ?>"></script>
-    <?php
-        endforeach;
-    endif;
-    ?>
     <?php require 'components/footer.php'; ?>
 </body>
 
