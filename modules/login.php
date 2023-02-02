10261c7430ff7f1cd7741e6b585a1cc7c632d0ec
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 03:08:13 +0100

    fix indentation and require call

--- modules/login.php
+++ modules/login.php
@@ -1,23 +1,22 @@
 <?php
 require_once 'bootstrap.php';
 
-if(isset($_POST['email'], $_POST['password'])) { 
-    $email = $_POST['email'];
-    $password = $_POST['password']; // Recupero la password criptata.
-    if(login($email, $password, $mysqli) == true) {
-       // Login eseguito
+if (isset($_POST['email'], $_POST['password'])) {
+   $email = $_POST['email'];
+   $password = $_POST['password']; // Recupero la password criptata.
+   if (login($email, $password, $mysqli) == true) {
+      // Login eseguito
       header('Location: ../index.php');
-    } else {
-       // Login fallito
-       $templateParams['errorerelogin'] = 'email o password errati';
-    }
- } else { 
-    // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
-    echo 'Invalid Request';
- }
+   } else {
+      // Login fallito
+      $templateParams['errorerelogin'] = 'email o password errati';
+   }
+} else {
+   // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
+   echo 'Invalid Request';
+}
 
 $templateParams["titolo"] = "Login | Memed";
-$templateParams["nome"]="../template/login.php";
+$templateParams["nome"] = "../template/login.php";
 
-require "../template/authentication.php";
-?>
+require "../template/baseUnlogged.php";
