26edb15aed948e94b61cae33d98a149ce112d6ce
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 04:06:35 +0100

    add function to validate emails

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -47,3 +47,16 @@ function checkPasswords(pwInput1, pwInput2) {
         registerButton.disabled = false;
     }
 }
+
+// function ValidateEmail(input) {
+//     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     if (input.value.match(validRegex)) {
+//         alert("Valid email address!");
+//         document.form1.text1.focus();
+//         return true;
+//     } else {
+//         alert("Invalid email address!");
+//         document.form1.text1.focus();
+//         return false;
+//     }
+// }
