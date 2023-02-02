0a79d1eae694c6242916402ac7e40cfe6b373fc3
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-02 12:28:11 +0100

    add comment posting handler

--- modules/index.php
+++ modules/index.php
@@ -1,13 +1,24 @@
 <?php
 require_once "bootstrap.php";
 
-if(login_check($mysqli) == true) {
+date_default_timezone_set('Europe/Rome');
+
+if (login_check($mysqli) == true) {
     $templateParams["titolo"] = "Memed";
     $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/index.js");
     $templateParams["username"] = $_SESSION["username"];
     $templateParams["posthome"] = $mysqli->ottieniPostPerHome($_SESSION["username"]);
     $templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
     $templateParams["notifiche"] = $mysqli->ottieniNotifica($_SESSION["username"]);
+    if (isset($_POST["post-comment"])) {
+        $mysqli->inserisciCommento(
+            $mysqli->ottieniIdUltimoCommento() + 1,
+            $_POST["comment-text"],
+            date("20y-m-d h:m:s"),
+            $_SESSION["username"],
+            $_POST["idpost"]
+        );
+    }
     require "../modules/base.php";
 } else {
     require "../modules/login.php";
