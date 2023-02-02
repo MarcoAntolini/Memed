363a38afc57eb8070ef28429ccb8e3f3921c6875
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-02 10:47:52 +0100

    fix index.php

--- modules/index.php
+++ modules/index.php
@@ -1,16 +1,17 @@
 <?php
 require_once "bootstrap.php";
 
-// if(login_check($mysqli) == true) {
+if(login_check($mysqli) == true) {
 $templateParams["titolo"] = "Memed";
-$templateParams["nome"] = "../template/registration.php"; //file da usare 
-// $templateParams["username"] = $_SESSION["username"];
-// $templateParams["posthome"] = $mysqli->ottieniPostPerHome($_SESSION["username"]);
-// $templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
-//  } else {
-//     echo 'You are not authorized to access this page, please login. <br/>';
-//     require "../template/login.php";
-//  }
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/index.js");
+$templateParams["username"] = $_SESSION["username"];
+$templateParams["posthome"] = $mysqli->ottieniPostPerHome($_SESSION["username"]);
+$templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
+$templateParams["notifiche"] = $mysqli->ottieniNotifica($_SESSION["username"]);
+} else {
+    echo 'You are not authorized to access this page, please login. <br/>';
+    require "../template/login.php";
+}
 
-// TODO: template base login/register
-require "../template/baseUnlogged.php";
+
+require "../template/base.php";
