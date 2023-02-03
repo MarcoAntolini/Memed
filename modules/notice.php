f86ee85c82ac266f827da4a095fa5d6bc1dd3f1a
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-02-03 16:13:14 +0100

    Update notice.php

--- modules/notice.php
+++ modules/notice.php
@@ -4,12 +4,18 @@ require_once 'bootstrap.php';
 // TODO: controllare se è desktop o mobile
 if (login_check($mysqli) == true) {
     $templateParams["titolo"] = "Memed - Notifiche";
-    $templateParams["nome"] = ""; //file da usare 
+    $templateParams["nome"] = "notice-view.php";
+    $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "../public/assets/js/noticeSection.js");
     $templateParams["username"] = $_SESSION["username"];
     $templateParams["numNotifiche"] = $mysqli->contaNotifiche($_SESSION["username"]);
     $templateParams["notifiche"] = $mysqli->ottieniNotifica($_SESSION["username"]);
+    if(isset($_POST["letto"])) {
+        $mysqli->leggiNotifica($_POST["letto"]);
+    }
+    if(isset($_POST["cancella"])) {
+        $mysqli->cancellaNotifica($_POST["cancella"]);
+    }
+    require 'template/logged-base-view.php';
 } else {
-    echo 'You are not authorized to access this page, please login. <br/>';
+    require "../modules/login.php";
 }
-
-require 'template/logged-base-view.php';
