040cce610660b3ab50d1501186a3adae84017888
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-03 23:55:18 +0100

    Update footer-view.php

--- template/components/footer-view.php
+++ template/components/footer-view.php
@@ -20,7 +20,7 @@
             </a>
         </div>
         <div class="col">
-            <a href="">
+            <a href="newPost.php">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <circle cx="12" cy="12" r="9" />
