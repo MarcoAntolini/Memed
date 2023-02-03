7b213628e1e76ed7206791a5baa537053fd55b69
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-03 21:55:41 +0100

    Update newPost.php

--- modules/newPost.php
+++ modules/newPost.php
@@ -5,6 +5,7 @@ if (login_check($mysqli) == true) {
     $templateParams["titolo"] = "Memed - Crea Post";
     $templateParams["nome"] = ""; //file da usare 
     $templateParams["username"] = $_SESSION["username"];
+    $templateParams["categorie"] = $dbh->getCategorie();
     require 'template/logged-base-view.php';
 } else {
     header("location: login.php");
