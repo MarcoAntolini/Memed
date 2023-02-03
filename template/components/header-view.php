841e664e52a3ec5bba0a68b47df99066e638fb17
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-03 17:59:33 +0100

    Add include input in form

--- template/components/header-view.php
+++ template/components/header-view.php
@@ -5,7 +5,9 @@
             <h1 class="title">Memed</h1>
         </div>
         <div class="search-bar col">
-            <input id="search" type="text" name="search" placeholder="Cerca" />
+            <form action="" name="search" method="post">
+                <input id="search" type="text" name="search" placeholder="Cerca" />
+            </form>
             <button class="btn">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
