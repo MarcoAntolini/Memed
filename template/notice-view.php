0084ced2f7840cb12492f8d5a7ceadb349edf6c8
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-03 18:41:06 +0100

    remove unused class

--- template/notice-view.php
+++ template/notice-view.php
@@ -1,4 +1,4 @@
-<section class="notice-section container-fluid d-flex justify-content-center p-2">
+<section class="container-fluid d-flex justify-content-center p-2">
     <div class="row">
         <div class="">
             <h2>Notifiche</h2>
@@ -6,7 +6,6 @@
             <button class="btn btn-primary" type="button" name="leggi-tutto">leggi tutto</button> <!-- segna tutte come lette -->
             <button class="btn btn-primary" type="button" name="cancella-tutto">cancella tutto</button>
         </div>
-
         <?php
         if (isset($templateParams["js"])) :
             echo '<div id="notice-section"></div>';
