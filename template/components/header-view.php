e50fd8120184c4aa1109d5699979f178256f9e54
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-03 02:22:54 +0100

    add logo

--- template/components/header-view.php
+++ template/components/header-view.php
@@ -1,14 +1,13 @@
 <div class="header container-fluid d-flex justify-content-center color-main p-2">
     <div class="row">
         <div class="logo col">
-            <img src="../public/assets/img/logo.png" alt="logo" />
+            <img src="../public/assets/img/logo.png" alt="logo" class="logo" />
             <h1>Memed</h1>
         </div>
         <div class="search-bar col">
             <input id="search" type="text" name="search" placeholder="Cerca" />
             <button class="btn">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24"
-                    stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <circle cx="10" cy="10" r="7" />
                     <line x1="21" y1="21" x2="15" y2="15" />
@@ -17,8 +16,7 @@
         </div>
         <div class="settings col">
             <button class="btn">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-align-center" width="44" height="44" viewBox="0 0 24 24"
-                    stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-align-center" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <line x1="4" y1="5" x2="20" y2="5" />
                     <line x1="4" y1="12" x2="20" y2="12" />
--- template/unlogged-base-view.php
+++ template/unlogged-base-view.php
@@ -5,18 +5,17 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="../public/assets/img/logo.ico" />
     <link rel="stylesheet" href="../public/assets/css/style.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="../public/assets/js/auth.js"></script>
     <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
 
 <body>
     <div class="header container-fluid d-flex justify-content-center color-main p-2">
-        <img src="" alt="logo" />
+        <img src="../public/assets/img/logo.png" alt="logo" class="logo" />
         <h1>Memed</h1>
     </div>
     <?php
@@ -24,7 +23,7 @@
         require $templateParams["nome"];
     }
     ?>
-    <div class="footer container-fluid d-flex justify-content-center color-main p-2">
+    <div class=" footer container-fluid d-flex justify-content-center color-main p-2">
         © 2023 Memed. Tutti i diritti riservati.
     </div>
 </body>
