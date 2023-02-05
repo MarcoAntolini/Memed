6e5e610ffc2f746e802cdabcf1f3c996f984bfae
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-05 19:55:50 +0100

    Create postPreview.js

---
+++ public/assets/js/postPreview.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    const imageInput = document.getElementById("image-input");
+    imageInput.addEventListener("change", function () {
+        previewImage(this.files[0]);
+    });
+    const descriptionInput = document.getElementById("description-input");
+    descriptionInput.addEventListener("keyup", function () {
+        previewDescription(this.value);
+    });
+};
+
+function previewImage(file) {
+    const validImageTypes = ["image/gif", "image/jpg", "image/jpeg", "image/png"];
+    if (file && validImageTypes.includes(file.type)) {
+        let oFReader = new FileReader();
+        oFReader.readAsDataURL(file);
+        oFReader.onload = function (oFREvent) {
+            const imagePreview = document.getElementById("image-preview");
+            const imageContent = `
+            <img src="${oFREvent.target.result}" alt="image-preview">
+            `;
+            imagePreview.innerHTML = imageContent;
+        };
+    }
+};
+
+function previewDescription(text) {
+    const descriptionPreview = document.getElementById("description-preview");
+    descriptionPreview.innerText = text;
+}
