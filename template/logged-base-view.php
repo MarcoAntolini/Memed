41695d0ccc5e63bda6576a92d50ad94c6a0b07cd
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-05 12:42:46 +0100

    Add jQuery Google CDN

--- template/logged-base-view.php
+++ template/logged-base-view.php
@@ -1,28 +1,26 @@
 <!DOCTYPE html>
 <html lang="it">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/x-icon" href="../public/assets/img/logo.ico" />
-    <link rel="stylesheet" href="../public/assets/css/style.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="../public/assets/js/search.js"></script>
-    <title><?php echo $templateParams["titolo"]; ?></title>
-</head>
-
-<body>
-    <?php require 'components/header-view.php'; ?>
-    <?php
-    if (isset($templateParams["nome"])) {
-        require $templateParams["nome"];
-    }
-    ?>
-    <?php require 'components/footer-view.php'; ?>
-</body>
-
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="../public/assets/img/logo.ico" />
+        <link rel="stylesheet" href="../public/assets/css/style.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script src="../public/assets/js/search.js"></script>
+        <title><?php echo $templateParams["titolo"]; ?></title>
+    </head>
+    <body>
+        <?php require 'components/header-view.php'; ?>
+        <?php
+        if (isset($templateParams["nome"])) {
+            require $templateParams["nome"];
+        }
+        ?>
+        <?php require 'components/footer-view.php'; ?>
+    </body>
 </html>
