40ab4032461abb5496f5ec82209732e907b9dbd1
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-05 12:40:43 +0100

    Implement function to get input content

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,11 +1,7 @@
-const form = document.querySelector('#search-form');
-const input = document.querySelector('#search');
-
-console.log(form);
-console.log(input);
-
-input.addEventListener("keydown", (e) => {
-    if (e.keyCode === 13) {
-        console.log(input.value);
-    }
-});
+$(document).ready(function() {
+    $('#search').keypress(function(e) {
+        if (e.keyCode === 13) {
+            console.log($('#search').val());
+        }
+    })
+})
