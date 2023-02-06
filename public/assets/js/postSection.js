ac7a65241e59a9ecd8913cf90145b11f4d05e006
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-06 15:51:51 +0100

    Update postSection.js

--- public/assets/js/postSection.js
+++ public/assets/js/postSection.js
@@ -5,7 +5,30 @@ function generatePost(post) {
         <div class="post container">
             <div class="left-up">
                 <a class="username-post-owner" href="user.php?username=${post[i]["username"]}">${post[i]["username"]}</a>
-                <img class="post-settings" src="" alt="">
+                <img src="" alt="" data-bs-toggle="modal" data-bs-target=".post-settings">
+                <div class="post-settings modal fade" role="dialog" data-bs-keyboard="false" data-bs-backdrop="static" tabindex="-1">
+                <div id="post-settings" class="modal-dialog modal-dialog-scrollable modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <form class="action-post" action="#" method="post">
+                            <input type="hidden" name="idpost" value="${post[i]["idpost"]}">
+                            <?php if ("${post[i]["username"]}" == $_SESSION["username"]): ?>
+                            <button class="delete-post btn btn-primary" type="button" name="edit-post">Modifica</button>
+                            <!-- TODO: il bottone qui sopra apre qualcosa per modificare il post -->
+                            <button class="delete-post btn btn-danger" type="submit" name="delete-post">Elimina</button>
+                            <?php else: ?>
+                            <button class="report-post btn btn-primary" type="submit" name="save-post">Salva</button>
+                            <button class="report-post btn btn-warning" type="submt" name="report-post">Segnala</button>
+                            <?php endif; ?>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                <form class="reaction-post" action="#" method="post">
                 <div class="reactions">
                     <div class="1">
                         <img src="../public/assets/img/reazione-1.png" alt="reazione-1">
@@ -28,6 +51,7 @@ function generatePost(post) {
                         <span class="reaction-count">${post[i]["reazione5"]}</span>
                     </div>
                 </div>
+                </form>
             </div>
             <div class="mid">
                 <img class="post-image" src="${post[i]["nomefile"]}" alt="">
