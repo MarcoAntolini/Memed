e6bc56bc31377987542a72dba2aa1bcaa9b69247
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-06 11:01:58 +0100

    fix bug

--- template/components/footer-view.php
+++ template/components/footer-view.php
@@ -42,7 +42,7 @@
             </a>
         </div>
         <div class="col">
-            <a href="user.php?username=<?php echo $templateParams["username"] ?>">
+            <a href="user.php?username=<?php echo $_SESSION["username"] ?>">
                 <svg xmlns=" http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
