0506ec1bb3f185ee171c10fe1fb34cc881578cae
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-06 13:10:40 +0100

    add logout function

--- modules/settings.php
+++ modules/settings.php
@@ -5,6 +5,12 @@ if (login_check($mysqli) == true) {
     $templateParams["titolo"] = "Memed - Impostazioni";
     $templateParams["nome"] = "settings-view.php";
     $templateParams["username"] = $_SESSION["username"];
+
+    if (isset($_POST["logout"])) {
+        logout();
+        header('location: login.php');
+    }
+
     require "../template/logged-base-view.php";
 } else {
     header("location: login.php");
--- modules/utils/functions.php
+++ modules/utils/functions.php
@@ -47,6 +47,14 @@ function login_check($mysqli)
     return true;
 }
 
+function logout()
+{
+    $_SESSION = array();
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    session_destroy();
+}
+
 function uploadImage($path, $image)
 {
     $imageName = basename($image["name"]);
