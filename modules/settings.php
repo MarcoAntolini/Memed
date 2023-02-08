1eff211ae4e83cbbe2836b3aa0a7b75e24ae88a2
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-08 17:29:35 +0100

    add call to database function

--- modules/settings.php
+++ modules/settings.php
@@ -11,6 +11,10 @@ if (login_check($mysqli) == true) {
         logout();
         header('location: login.php');
     }
+    if (isset($_POST["submit"])) {
+        $mysqli->modificaProfilo($_SESSION["username"], $_POST["bio"], $_POST["profile-pic-input"]);
+        header('location: user.php?username=' . $_SESSION["username"] . '');
+    }
     require "../template/logged-base-view.php";
 } else {
     header("location: login.php");
