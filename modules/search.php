4b6d7233f7ee8d00bc24a80e6d59abc4760469d1
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-08 15:27:05 +0100

    Add search.php

---
+++ modules/search.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once "../modules/bootstrap.php";
+    $templateParams["titolo"] = "Memed - Ricerca";
+    $templateParams["risultati"] = $mysqli->ottieniUtentiPerNome($_GET['search']);
+    require "../template/logged-base-view.php";
+    require "../template/search-view.php";
+    /* TODO:
+        add onclick='follow()' to button
+        add href to username
+        add bio
+        add photo (in the db tho) and replace the svg
+        add js to disable input if empty
+    */
