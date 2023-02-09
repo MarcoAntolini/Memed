2d1434af5295d6d1e1fcc49ca2769b526f0d063b
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-09 17:12:49 +0100

    add style classes

--- template/components/footer-view.php
+++ template/components/footer-view.php
@@ -2,27 +2,27 @@
     <div class="row gap-2">
         <div class="col">
             <a href="index.php">
-                <img src="../public/assets/img/home.png" alt="homepage">
+                <img src="../public/assets/img/home.png" alt="homepage" class="nav-icon">
             </a>
         </div>
         <div class="col">
             <a href="explore.php">
-                <img src="../public/assets/img/explore.png" alt="explore">
+                <img src="../public/assets/img/explore.png" alt="explore" class="nav-icon">
             </a>
         </div>
         <div class="col">
             <a href="newPost.php">
-                <img src="../public/assets/img/new-post.png" alt="new-post">
+                <img src="../public/assets/img/new-post.png" alt="new-post" class="nav-icon">
             </a>
         </div>
         <div class="col">
             <a href="notice.php">
-                <img src="../public/assets/img/notices.png" alt="notices">
+                <img src="../public/assets/img/notices.png" alt="notices" class="nav-icon">
             </a>
         </div>
         <div class="col">
             <a href="user.php?username=<?php echo $_SESSION["username"] ?>">
-                <img src="../public/assets/img/profile.png" alt="profile">
+                <img src="../public/assets/img/profile.png" alt="profile" class="nav-icon">
             </a>
         </div>
     </div>
