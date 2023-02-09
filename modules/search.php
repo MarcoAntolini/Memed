9f9907e7d47cbe8749344a692a45808b4de23b63
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-09 17:52:28 +0100

    Improve code

--- modules/search.php
+++ modules/search.php
@@ -1,9 +1,11 @@
 <?php
-require_once "../modules/bootstrap.php";
-$templateParams["titolo"] = "Memed - Cerca";
-$templateParams["risultati"] = $mysqli->ottieniUtentiPerNome($_GET['search']);
-require "../template/logged-base-view.php";
-require "../template/search-view.php";
+    require_once "../modules/bootstrap.php";
+    $templateParams["titolo"] = "Memed - Cerca";
+    $templateParams["risultati"] = $mysqli->ottieniUtentiPerNome($_GET['search']);
+    $res = $templateParams["risultati"];
+    $currUser = $_SESSION["username"];
+    require "../template/logged-base-view.php";
+    require "../template/search-view.php";
     /* TODO:
         add onclick='follow()' to button
     */
