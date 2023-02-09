603b3512aad1a13857995c22f4ceb7e984dbb56f
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-09 15:40:53 +0100

    remove svgs

--- template/components/header-view.php
+++ template/components/header-view.php
@@ -12,11 +12,7 @@
                         <label for="search-input" class="form-label">Cerca</label>
                     </div>
                     <button id="search-button" type="submit" class="btn btn-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="white" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="" />
-                            <circle cx="10" cy="10" r="7" />
-                            <line x1="21" y1="21" x2="15" y2="15" />
-                        </svg>
+                        <img src="../public/assets/img/search.png" alt="search">
                     </button>
                 </div>
             </form>
@@ -24,12 +20,7 @@
         <div class="col">
             <a href="settings.php">
                 <button class="btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-align-center" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="4" y1="5" x2="20" y2="5" />
-                        <line x1="4" y1="12" x2="20" y2="12" />
-                        <line x1="4" y1="19" x2="20" y2="19" />
-                    </svg>
+                    <img src="../public/assets/img/settings.png" alt="settings">
                 </button>
             </a>
         </div>
