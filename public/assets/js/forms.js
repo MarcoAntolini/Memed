571d3ee33b26f8dafc9b3bef4913394f88e630f8
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-11 14:10:03 +0100

    remove unused code

--- public/assets/js/forms.js
+++
@@ -1,9 +0,0 @@
-function formhash(form, password) {
-    var p = document.createElement("input");
-    form.appendChild(p);
-    p.name = "p";
-    p.type = "hidden";
-    p.value = hex_sha512(password.value);
-    password.value = "";
-    form.submit();
-}
