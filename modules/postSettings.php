6a7a1de05ec9e33c8e5ebc5baaa437b39ef445cb
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-02-12 16:51:52 +0100

    commenti con solo spazi no

--- modules/postSettings.php
+++ modules/postSettings.php
@@ -14,7 +14,7 @@ if (isset($_POST["unsave-post"])) {
     $mysqli->cancellaSalva($_SESSION["username"], $_POST["idpost"]);
 }
 if (isset($_POST["submit-comment"])) {
-    if (empty($_POST["comment-text"]) || $_POST["comment-text"]=="" || preg_match('/^[\s]+$/', $string)) {
+    if (empty($_POST["comment-text"]) || $_POST["comment-text"]=="" || preg_match("/^[\s]+$/", $_POST["comment-text"])) {
         return;
     } else {
         $mysqli->inserisciCommento(
