f32e79e72ea4f96944d239bb7137cab6aecc56ad
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2023-02-12 11:57:59 +0100

    Add new script and move footer outside the main

--- template/logged-base-view.php
+++ template/logged-base-view.php
@@ -15,6 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="../public/assets/js/search.js"></script>
+    <script src="../public/assets/js/search-input.js"></script>
     <script src="../public/assets/js/redirect.js"></script>
     <title><?php echo $templateParams["titolo"]; ?></title>
 </head>
@@ -35,8 +36,8 @@
         }
         ?>
 
-        <?php require 'components/footer-view.php'; ?>
     </main>
+    <?php require 'components/footer-view.php'; ?>
 </body>
 
 </html>
