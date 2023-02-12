6f8f5c5eca45a356e029ccb38036df2cffdaf7eb
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-12 15:08:25 +0100

    disable buttons instead of hiding them

--- public/assets/js/notifications.js
+++ public/assets/js/notifications.js
@@ -1,7 +1,7 @@
-$(function() {
+$(function () {
     const notifications = $('#notifications').val();
     if (notifications == 0) {
-        $('#clearall').hide()
-        $('#readall').hide()
+        $('#clearall').prop("disabled", true);
+        $('#readall').prop("disabled", true);
     }
 });
