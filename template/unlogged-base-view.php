5cb3cb28224ca05b7f34cebc986fffed89b3af7e
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-13 20:06:58 +0100

    add style to unlogged pages

--- template/unlogged-base-view.php
+++ template/unlogged-base-view.php
@@ -24,7 +24,7 @@
             <h1 class="title">Memed</h1>
         </div>
     </header>
-    <main class="vw-100">
+    <main class="auth vw-100">
         <?php
         if (isset($templateParams["nome"])) {
             require $templateParams["nome"];
