d93372e6f831b0fafb910ef489a0e0d75d46a551
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-14 00:15:23 +0100

    fix explore style

--- template/explore-view.php
+++ template/explore-view.php
@@ -1,15 +1,20 @@
-<div class="container d-flex gap-2">
-    <form action="#" method="post" class="d-flex gap-2">
+<div class="explore-section">
+    <form action="#" method="post" class="d-flex gap-3 align-items-center">
         <?php foreach ($templateParams["categorie"] as $categoria) : ?>
-            <input type="radio" id="<?php echo $categoria["idcategoria"]; ?>" name="categoria" value="<?php echo $categoria["idcategoria"]; ?>" />
-            <label for="<?php echo $categoria["idcategoria"]; ?>"><?php echo $categoria["nome"]; ?></label>
+            <div class="category">
+                <input type="radio" id="<?php echo $categoria["idcategoria"]; ?>" name="categoria" value="<?php echo $categoria["idcategoria"]; ?>" />
+                <label for="<?php echo $categoria["idcategoria"]; ?>"><?php echo $categoria["nome"]; ?></label>
+            </div>
         <?php endforeach; ?>
-        <input type="radio" id="0" name="categoria" value="0" />
-        <label for="0">tutte</label>
+        <div class="category">
+            <input type="radio" id="0" name="categoria" value="0" />
+            <label for="0">Tutte</label>
+        </div>
         <button type="submit" class="btn btn-primary">Filtra</button>
+    </form>
+    <?php
+    if (isset($templateParams["js"])) {
+        echo '<div id="post-section"></div>';
+    }
+    ?>
 </div>
-<?php
-if (isset($templateParams["js"])) {
-    echo '<div id="post-section"></div>';
-}
-?>
