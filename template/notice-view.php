303f57abe25ad69429c92488339d2316aac82443
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-14 04:40:20 +0100

    fix notice param

--- template/notice-view.php
+++ template/notice-view.php
@@ -2,15 +2,10 @@
     <div class=" row">
     <form action="#" method="post">
         <div>
-            <p class="fw-bold">Hai
-                <?php
-                $notifications = $templateParams["numNotifiche"];
-                echo $notifications;
-                ?> notifiche non lette
-            </p>
+            <p class="fw-bold">Hai <?php echo $templateParams["numNotifiche"]; ?> notifiche non lette</p>
             <button id="readall" class="btn btn-success" type="submit" name="leggi-tutto">Leggi tutto</button> <!-- segna tutte come lette -->
             <button id="clearall" class="btn btn-danger" type="submit" name="cancella-tutto">Cancella tutto</button>
-            <input type="hidden" id="notifications" value="<?php echo $notifications; ?>">
+            <input type="hidden" id="notifications" value="<?php echo $templateParams["numNotifiche"]; ?>">
             <input type="hidden" id="notifiche" value="<?php echo $templateParams["notifiche"]; ?>">
         </div>
     </form>
