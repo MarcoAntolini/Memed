a6e9c9c2f80a9990d4309d6a8c1dc2fe5fe8a97a
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-14 03:06:13 +0100

    fix search style

--- template/search-view.php
+++ template/search-view.php
@@ -1,24 +1,27 @@
-<?php if (empty($res)) : ?>
-    <h2 class="text-center">Nessun risultato</h2>
-<?php else : ?>
-    <?php foreach ($res as $r) : ?>
-        <?php if ($r["username"] !== $currUser) : ?>
-            <div class="d-flex justify-content-between border border-dark rounded bg-light text-dark p-3 m-2">
-                <div class="d-flex align-items-center" style="width:40vw;">
-                    <img src="<?php echo (UPLOAD_DIR . $r["nomefile"]) ?>" alt="profile-picture" style="width:3vw; height:3vw" />
+<div class="search-container">
+    <?php if (empty($res)) : ?>
+        <h2 class="text-center">Nessun risultato</h2>
+    <?php else : ?>
+        <?php foreach ($res as $r) : ?>
+            <?php if ($r["username"] !== $currUser) : ?>
+                <div class="card p-2 mb-3">
                     <div class="">
-                        <span class="font-weight-bold"><a href="user.php?username=<?php echo $r["username"] ?>">@<?php echo $r["username"] ?></a><br></span>
-                        <span class="text-muted"><?php echo $r["bio"] ?></span>
+                        <img src="<?php echo (UPLOAD_DIR . $r["nomefile"]) ?>" alt="profile-picture" />
+                        <div class="">
+                            <h2 class="fw-bold"><a href="user.php?username=<?php echo $r["username"] ?>">@<?php echo $r["username"] ?></a><br></h2>
+                            <p class="text-muted"><?php echo $r["bio"] ?></p>
+                        </div>
                     </div>
+                    <?php if ($mysqli->controllaSegue($r["username"], $currUser)) : ?>
+                        <button type="button" id="<?php echo $r["username"] ?>" class="btn btn-primary">Smetti di seguire</button>
                 </div>
-                <?php if ($mysqli->controllaSegue($r["username"], $currUser)) : ?>
-                    <button type="button" id="<?php echo $r["username"] ?>" class="followBtn btn btn-primary btn-sm btn-block" style="width:5vw">Smetti di seguire</button>
-            </div>
-        <?php else : ?>
-            <button type="button" id="" . $r["username"] . "" class="followBtn btn btn-primary btn-sm btn-block" style="width:5vw">Segui</button>
-            </div>
+            <?php else : ?>
+                <button type="button" id="<?php echo $r["username"] ?>" class="btn btn-primary">Segui</button>
+</div>
 <?php
+                    endif;
                 endif;
-            endif;
-        endforeach;
-    endif;
+            endforeach;
+        endif;
+?>
+</div>
