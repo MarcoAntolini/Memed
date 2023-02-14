530f25d7b00e58988d0ae6797c50931ebe3c54d0
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-14 02:42:29 +0100

    fix search view

--- template/search-view.php
+++ template/search-view.php
@@ -1,42 +1,24 @@
+<?php if (empty($res)) : ?>
+    <h2 class="text-center">Nessun risultato</h2>
+<?php else : ?>
+    <?php foreach ($res as $r) : ?>
+        <?php if ($r["username"] !== $currUser) : ?>
+            <div class="d-flex justify-content-between border border-dark rounded bg-light text-dark p-3 m-2">
+                <div class="d-flex align-items-center" style="width:40vw;">
+                    <img src="<?php echo (UPLOAD_DIR . $r["nomefile"]) ?>" alt="profile-picture" style="width:3vw; height:3vw" />
+                    <div class="">
+                        <span class="font-weight-bold"><a href="user.php?username=<?php echo $r["username"] ?>">@<?php echo $r["username"] ?></a><br></span>
+                        <span class="text-muted"><?php echo $r["bio"] ?></span>
+                    </div>
+                </div>
+                <?php if ($mysqli->controllaSegue($r["username"], $currUser)) : ?>
+                    <button type="button" id="<?php echo $r["username"] ?>" class="followBtn btn btn-primary btn-sm btn-block" style="width:5vw">Smetti di seguire</button>
+            </div>
+        <?php else : ?>
+            <button type="button" id="" . $r["username"] . "" class="followBtn btn btn-primary btn-sm btn-block" style="width:5vw">Segui</button>
+            </div>
 <?php
-if (empty($res)) {
-    echo "<h1 class='text-center'>Nessun risultato</h1>";
-} else {
-    foreach ($res as $r) {
-        if ($r["username"] !== $currUser) {
-            if ($mysqli->controllaSegue($r['username'], $currUser)) {
-                echo
-                "<div class='d-flex justify-content-between border border-dark rounded bg-light text-dark p-3 m-2'>
-                        <div class='d-flex align-items-center' style='width:40vw;'>
-                            <img src='" . UPLOAD_DIR . $r['nomefile'] . "' alt='' style='width:3vw; height:3vw' />
-                            <div class=''>
-                                <span class='font-weight-bold'>
-                                    <a href='user.php?username=" . $r['username'] . "'>" . "@" . $r["username"] . "</a><br>
-                                </span>
-                                <span class='text-muted'>
-                                    " . $r['bio'] . "
-                                </span>
-                            </div>
-                        </div>
-                        <button type='button' id='" . $r['username'] . "' class='followBtn btn btn-primary btn-sm btn-block' style='width:5vw'>Smetti di seguire</button>
-                    </div>";
-            } else {
-                echo
-                "<div class='d-flex justify-content-between border border-dark rounded bg-light text-dark p-3 m-2'>
-                        <div class='d-flex align-items-center' style='width:40vw;'>
-                            <img src='" . UPLOAD_DIR . $r['nomefile'] . "' alt='' style='width:3vw; height:3vw' />
-                            <div class=''>
-                                <span class='font-weight-bold'>
-                                    <a href='user.php?username=" . $r['username'] . "'>" . "@" . $r["username"] . "</a><br>
-                                </span>
-                                <span class='text-muted'>
-                                    " . $r['bio'] . "
-                                </span>
-                            </div>
-                        </div>
-                        <button type='button' id='" . $r['username'] . "' class='followBtn btn btn-primary btn-sm btn-block' style='width:5vw'>Segui</button>
-                    </div>";
-            }
-        }
-    }
-}
+                endif;
+            endif;
+        endforeach;
+    endif;
