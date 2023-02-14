1e8e9380cfa6ffb55a3f84d3dec4139d782f0c81
MarcoAntolini <marco.antolini2001@gmail.com>
2023-02-14 02:27:45 +0100

    fix eplore style

--- template/explore-view.php
+++ template/explore-view.php
@@ -1,16 +1,9 @@
 <div class="explore-section">
     <form action="#" method="post" class="d-flex gap-3 align-items-center">
         <?php foreach ($templateParams["categorie"] as $categoria) : ?>
-            <div class="category">
-                <input type="radio" id="<?php echo $categoria["idcategoria"]; ?>" name="categoria" value="<?php echo $categoria["idcategoria"]; ?>" />
-                <label for="<?php echo $categoria["idcategoria"]; ?>"><?php echo $categoria["nome"]; ?></label>
-            </div>
+            <button type="submit" id="<?php echo $categoria["idcategoria"]; ?>" name="categoria" value="<?php echo $categoria["idcategoria"]; ?>" class="category btn btn-outline-primary" /><?php echo $categoria["nome"]; ?></button>
         <?php endforeach; ?>
-        <div class="category">
-            <input type="radio" id="0" name="categoria" value="0" />
-            <label for="0">Tutte</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Filtra</button>
+        <button type="submit" id="0" name="categoria" value="0" class="category btn btn-outline-primary" />Tutti</button>
     </form>
     <?php
     if (isset($templateParams["js"])) {
