073849a557fd7c400d31a46f2b60c3bbc83958b9
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-20 04:20:26 +0100

    convert code from jquery to plain javascript

--- public/assets/js/notifications.js
+++ public/assets/js/notifications.js
@@ -1,10 +1,10 @@
-$(function () {
-    const notifications = $('#notifications').val();
-    const notifiche = $('#notifiche').val();
-    if (notifications == 0) {
-        $('#readall').prop("disabled", true);
-    }
-    if (!notifiche) {
-        $('#clearall').prop("disabled", true);
-    }
-});
+window.onload = () => {
+	const notifications = document.getElementById("notifications-number")
+	const notificationsNumber = notifications ? notifications.value : 0
+	const notification = document.getElementById("notification-Id")
+	const notificationId = notification ? notification.value : false
+	const readAllButton = document.getElementById("readall")
+	const clearAllButton = document.getElementById("clearall")
+	if (notificationsNumber == 0 && readAllButton) readAllButton.disabled = true
+	if (!notificationId && clearAllButton) clearAllButton.disabled = true
+}
