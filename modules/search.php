d7e5038b2b0f8f05bd6f06ef0916d665e1610130
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-21 03:32:26 +0100

    refactor code, fix bugs and connect to database

--- modules/search.php
+++ modules/search.php
@@ -4,6 +4,12 @@ $templateParams["titolo"] = "Memed - Cerca";
 $templateParams["nome"] = "search-view.php";
 $templateParams["risultati"] = $mysqli->getUserLikeUsername($_GET['search']);
 $templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "../public/assets/js/noticeSection.js");
-$res = $templateParams["risultati"];
+$usersList = $templateParams["risultati"];
 $currUser = $_SESSION["Username"];
+if (isset($_POST["unfollowing"])) {
+	$mysqli->deleteFollow($_POST["unfollowing"], $_SESSION["Username"]);
+}
+if (isset($_POST["following"])) {
+	$mysqli->insertFollow($_POST["following"], $_SESSION["Username"]);
+}
 require "../template/logged-base-view.php";
