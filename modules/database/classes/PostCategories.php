06b8f84c1ddeb8bb696a92a948f0d358ca1a7452
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-26 23:50:38 +0200

    add column methods

--- modules/database/classes/PostCategories.php
+++ modules/database/classes/PostCategories.php
@@ -8,4 +8,34 @@ class PostCategories
 	{
 		$this->db = $db;
 	}
+
+	public function insertPostCategory($categoryId, $postId)
+	{
+		$sql = "INSERT INTO post_categories (PostID, CategoryID) VALUES (?, ?)";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("ii", $postId, $categoryId);
+		$stmt->execute();
+	}
+
+	// public function ottienicategoriePost($postId)
+	// {
+	//     $sql = "SELECT * FROM post_categories WHERE PostID = ?";
+	//     $stmt = $this->db->prepare($sql);
+	//     $stmt->bind_param("i", $postId);
+	//     $stmt->execute();
+	//     $result = $stmt->get_result();
+	//     if ($result->num_rows > 0) {
+	//         return $result->fetch_all(MYSQLI_ASSOC);
+	//     } else {
+	//         return false;
+	//     }
+	// }
+
+	public function deleteAllCategoriesFromPost($postId)
+	{
+		$sql = "DELETE FROM post_categories WHERE PostID = ?";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("i", $postId);
+		$stmt->execute();
+	}
 }
