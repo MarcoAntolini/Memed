d73494d1341b38e018b8dd488ff3667d85477c0d
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-26 23:50:00 +0200

    add column methods

--- modules/database/classes/SavedPosts.php
+++ modules/database/classes/SavedPosts.php
@@ -8,4 +8,53 @@ class SavedPosts
 	{
 		$this->db = $db;
 	}
+
+	public function insertSavedPost($username, $postId)
+	{
+		$sql = "INSERT INTO saved_posts (PostID, Username) VALUES (?, ?)";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("is", $postId, $username);
+		$stmt->execute();
+	}
+
+	public function checkSavedPost($username, $postId)
+	{
+		$sql = "SELECT * FROM saved_posts WHERE Username = ? AND PostID = ?";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("si", $username, $postId);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		return $result->num_rows > 0;
+	}
+
+	public function deleteSavedPost($username, $postId)
+	{
+		$sql = "DELETE FROM saved_posts WHERE Username = ? AND PostID = ?";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("si", $username, $postId);
+		$stmt->execute();
+	}
+
+	public function getSavedPostsByUsername($username)
+	{
+		$sql = "SELECT * FROM posts WHERE PostID IN (SELECT PostID FROM saved_posts WHERE Username = ?) ORDER BY DateAndTime DESC";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("s", $username);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if ($result->num_rows > 0) {
+			return $result->fetch_all(MYSQLI_ASSOC);
+		} else {
+			// TODO: return false?
+			return false;
+		}
+	}
+
+	public function deleteSavedPostById($postId)
+	{
+		$sql = "DELETE FROM saved_posts WHERE PostID = ?";
+		$stmt = $this->db->prepare($sql);
+		$stmt->bind_param("i", $postId);
+		$stmt->execute();
+	}
 }
