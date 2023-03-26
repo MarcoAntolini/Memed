4f09308918daaa6136342ba6d38e19aa6666ce79
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-26 23:50:23 +0200

    add column methods

--- modules/database/classes/Categories.php
+++ modules/database/classes/Categories.php
@@ -8,4 +8,25 @@ class Categories
 	{
 		$this->db = $db;
 	}
+
+	// public function inserisciCategoria($Name, $categoryId)
+	// {
+	//     $sql = "INSERT INTO categories (CategoryID, Name) VALUES (?, ?)";
+	//     $stmt = $this->db->prepare($sql);
+	//     $stmt->bind_param("is", $categoryId, $Name);
+	//     $stmt->execute();
+	// }
+
+	public function getCategories()
+	{
+		$sql = "SELECT * FROM categories";
+		$stmt = $this->db->prepare($sql);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		// if ($result->num_rows > 0) {
+		return $result->fetch_all(MYSQLI_ASSOC);
+		// } else {
+		//     return false;
+		// }
+	}
 }
