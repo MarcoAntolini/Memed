97e1b65dcf18105aaa52923e6fc6a8a3500844e9
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-26 23:49:34 +0200

    Update database.php

--- modules/database/database.php
+++ modules/database/database.php
@@ -626,7 +626,6 @@ class DatabaseHelper
         $stmt->execute();
     }
 
-
     public function getFileNameByUsername($username)
     {
         $sql = "SELECT FileName FROM users WHERE Username = ?";
