a0d0ac420fd6416c17caf9c3dc34c85a335a93c2
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-28 18:23:00 +0200

    format code, add return and parameters types

--- modules/utils/functions.php
+++ modules/utils/functions.php
@@ -1,79 +1,79 @@
 <?php
 
-function registerLoggedUser($user)
+function registerLoggedUser(string $user)
 {
-    $_SESSION["Username"] = $user["Username"];
+	$_SESSION["Username"] = $user["Username"];
 }
 
-function login($email, $password, $mysqli)
+function login(string $email, string $password, DatabaseHelper $mysqli): bool
 {
-    if (!$user = $mysqli->getUserByEmail($email)) return false;
-    $username = $user[0]['Username'];
-    $db_password = $user[0]['Password'];
-    $salt = $user[0]['PasswordSalt'];
-    $password = hash('sha512', $password . $salt);
-    if ($db_password != $password) return false;
-    $user_browser = $_SERVER['HTTP_USER_AGENT'];
-    $_SESSION['Username'] = $username;
-    $_SESSION['login_string'] = hash('sha512', $password . $user_browser);
-    return true;
+	if (!$user = $mysqli->getUserByEmail($email)) return false;
+	$username = $user[0]['Username'];
+	$db_password = $user[0]['Password'];
+	$salt = $user[0]['PasswordSalt'];
+	$password = hash('sha512', $password . $salt);
+	if ($db_password != $password) return false;
+	$user_browser = $_SERVER['HTTP_USER_AGENT'];
+	$_SESSION['Username'] = $username;
+	$_SESSION['login_string'] = hash('sha512', $password . $user_browser);
+	return true;
 }
 
-function login_check($mysqli)
+function login_check(DatabaseHelper $mysqli): bool
 {
-    if (!isset($_SESSION['Username'], $_SESSION['login_string'])) return false;
-    $login_string = $_SESSION['login_string'];
-    $username = $_SESSION['Username'];
-    $user_browser = $_SERVER['HTTP_USER_AGENT'];
-    if (!$stmt = $mysqli->getUserByUsername($username)) return false;
-    $password = $stmt[0]['Password'];
-    $login_check = hash('sha512', $password . $user_browser);
-    if ($login_check != $login_string) return false;
-    return true;
+	if (!isset($_SESSION['Username'], $_SESSION['login_string'])) return false;
+	$login_string = $_SESSION['login_string'];
+	$username = $_SESSION['Username'];
+	$user_browser = $_SERVER['HTTP_USER_AGENT'];
+	if (!$stmt = $mysqli->getUserByUsername($username)) return false;
+	$password = $stmt[0]['Password'];
+	$login_check = hash('sha512', $password . $user_browser);
+	if ($login_check != $login_string) return false;
+	return true;
 }
 
-function logout()
+function logout(): void
 {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-    session_destroy();
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+	session_destroy();
 }
 
-function uploadImage($path, $image)
+function uploadImage(string $path, array $image): array
 {
-    $imageName = basename($image["name"]);
-    $fullPath = $path . $imageName;
+	$imageName = basename($image["name"]);
+	$fullPath = $path . $imageName;
 
-    $maxKB = 5000;
-    $acceptedExtensions = array("jpg", "jpeg", "png", "gif");
-    $result = 0;
-    $msg = "";
-    $imageSize = getimagesize($image["tmp_name"]);
-    if ($imageSize === false) {
-        $msg .= "File caricato non è un'immagine! ";
-    }
-    if ($image["size"] > $maxKB * 1024) {
-        $msg .= "File caricato pesa troppo! Dimensione massima è $maxKB KB. ";
-    }
-    $imageFileType = strtolower(pathinfo($fullPath, PATHINFO_EXTENSION));
-    if (!in_array($imageFileType, $acceptedExtensions)) {
-        $msg .= "Accettate solo le seguenti estensioni: " . implode(",", $acceptedExtensions);
-    }
-    if (file_exists($fullPath)) {
-        $i = 1;
-        do {
-            $i++;
-            $imageName = pathinfo(basename($image["name"]), PATHINFO_FILENAME) . "_$i." . $imageFileType;
-        } while (file_exists($path . $imageName));
-        $fullPath = $path . $imageName;
-    }
-    if (strlen($msg) == 0) {
-        if (!move_uploaded_file($image["tmp_name"], $fullPath)) {
-            $msg .= "Errore nel caricamento dell'immagine.";
-        } else {
-            $result = 1;
-            $msg = $imageName;
-        }
-    }
-    return array($result, $msg);
+	$maxKB = 5000;
+	$acceptedExtensions = array("jpg", "jpeg", "png", "gif");
+	$result = 0;
+	$msg = "";
+	$imageSize = getimagesize($image["tmp_name"]);
+	if ($imageSize === false) {
+		$msg .= "File caricato non è un'immagine! ";
+	}
+	if ($image["size"] > $maxKB * 1024) {
+		$msg .= "File caricato pesa troppo! Dimensione massima è $maxKB KB. ";
+	}
+	$imageFileType = strtolower(pathinfo($fullPath, PATHINFO_EXTENSION));
+	if (!in_array($imageFileType, $acceptedExtensions)) {
+		$msg .= "Accettate solo le seguenti estensioni: " . implode(",", $acceptedExtensions);
+	}
+	if (file_exists($fullPath)) {
+		$i = 1;
+		do {
+			$i++;
+			$imageName = pathinfo(basename($image["name"]), PATHINFO_FILENAME) . "_$i." . $imageFileType;
+		} while (file_exists($path . $imageName));
+		$fullPath = $path . $imageName;
+	}
+	if (strlen($msg) == 0) {
+		if (!move_uploaded_file($image["tmp_name"], $fullPath)) {
+			$msg .= "Errore nel caricamento dell'immagine.";
+		} else {
+			$result = 1;
+			$msg = $imageName;
+		}
+	}
+	return array($result, $msg);
 }
