3e34b5c2ac014a644db14a6f0d90eadcc0d608d6
Marco Antolini <marco.antolini2001@gmail.com>
2023-03-28 18:52:09 +0200

    rename folder

--- modules/bootstrap.php
+++ modules/bootstrap.php
@@ -3,7 +3,7 @@
 session_start();
 define("UPLOAD_DIR", "./upload/");
 
-require_once("utils/*");
+require_once("functions/*");
 require_once("database/DatabaseHelper.php");
 
 $mysqli = new DatabaseHelper("localhost", "root", "", "memed", 3306);
