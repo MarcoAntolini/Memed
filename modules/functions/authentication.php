a11a9799662689e4551972db7f9c1d76cf4903c8
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-30 01:09:35 +0200

    format code

--- modules/functions/authentication.php
+++ modules/functions/authentication.php
@@ -10,32 +10,32 @@ function login(string $email, string $password, DatabaseHelper $mysqli): bool
 	if (!$user = $mysqli->users()->getUserByEmail($email)) {
 		return false;
 	}
-	$username = $user[0]['Username'];
-	$userPassword = $user[0]['Password'];
-	$userSalt = $user[0]['PasswordSalt'];
-	$password = hash('sha512', $password . $userSalt);
+	$username = $user[0]["Username"];
+	$userPassword = $user[0]["Password"];
+	$userSalt = $user[0]["PasswordSalt"];
+	$password = hash("sha512", $password . $userSalt);
 	if ($userPassword != $password) {
 		return false;
 	}
-	$browser = $_SERVER['HTTP_USER_AGENT'];
-	$_SESSION['Username'] = $username;
-	$_SESSION['login_string'] = hash('sha512', $password . $browser);
+	$browser = $_SERVER["HTTP_USER_AGENT"];
+	$_SESSION["LoggedUser"] = $username;
+	$_SESSION["login_string"] = hash("sha512", $password . $browser);
 	return true;
 }
 
 function checkLogin(DatabaseHelper $mysqli): bool
 {
-	if (!isset($_SESSION['Username'], $_SESSION['login_string'])) {
+	if (!isset($_SESSION["LoggedUser"], $_SESSION["login_string"])) {
 		return false;
 	}
-	$loginString = $_SESSION['login_string'];
-	$username = $_SESSION['Username'];
-	$browser = $_SERVER['HTTP_USER_AGENT'];
+	$loginString = $_SESSION["login_string"];
+	$username = $_SESSION["LoggedUser"];
+	$browser = $_SERVER["HTTP_USER_AGENT"];
 	if (!$user = $mysqli->users()->getUserByUsername($username)) {
 		return false;
 	}
-	$password = $user[0]['Password'];
-	$checkLogin = hash('sha512', $password . $browser);
+	$password = $user[0]["Password"];
+	$checkLogin = hash("sha512", $password . $browser);
 	if ($checkLogin != $loginString) {
 		return false;
 	}
@@ -47,7 +47,7 @@ function logout(): void
 	$params = session_get_cookie_params();
 	setcookie(
 		session_name(),
-		'',
+		"",
 		time() - 42000,
 		$params["path"],
 		$params["domain"],
