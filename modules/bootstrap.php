73ab75c97d47e27b09af680ebc64e5aeccb19138
MarcoAntolini <marco.antolini2001@gmail.com>
2023-03-30 01:08:54 +0200

    format code

--- modules/bootstrap.php
+++ modules/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 
+require_once "database/DatabaseHelper.php";
+require_once "functions/authentication.php";
+require_once "functions/upload.php";
+
 session_start();
 define("UPLOAD_DIR", "./upload/");
 define("INDEX", "location: index.php");
 define("LOGIN", "location: login.php");
-
-require_once "database/DatabaseHelper.php";
-require_once "functions/authentication.php";
-require_once "functions/upload.php";
 
 $mysqli = new DatabaseHelper("localhost", "root", "", "memed", 3306);
