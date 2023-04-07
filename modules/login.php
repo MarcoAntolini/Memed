6d28f27d7ec053a8dfd2b8d34a68be3d1ce2654c
MarcoAntolini <marco.antolini2001@gmail.com>
2023-04-07 18:35:09 +0200

    fix typo

--- modules/login.php
+++ modules/login.php
@@ -11,7 +11,7 @@ if (isset($_POST["email"], $_POST["password"])) {
 	if (login($email, $password, $mysqli)) {
 		header(INDEX);
 	} else {
-		$templateParams["loginErrorMessage"] = "email o password errati";
+		$templateParams["loginErrorMessage"] = "Email o password errati";
 	}
 }
 
