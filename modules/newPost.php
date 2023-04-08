5f9740f701ddbbd95ac7186cc035b3c84703b6fa
MarcoAntolini <marco.antolini2001@gmail.com>
2023-04-08 04:26:21 +0200

    format code

--- modules/newPost.php
+++ modules/newPost.php
@@ -5,7 +5,10 @@ require_once "sessionCheck.php";
 
 $templateParams["title"] = "Memed - Crea Post";
 $templateParams["page"] = "newPost.php";
-$templateParams["js"] = array("https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js", "../public/assets/js/notificationSection.js");
+$templateParams["js"] = array(
+	"https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js",
+	"../public/assets/js/notificationSection.js"
+);
 
 $templateParams["categories"] = $mysqli->categories()->getCategories();
 
