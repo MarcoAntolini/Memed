5322ea07592307e2494a51fa975558fe32834b24
MarcoAntolini <marco.antolini2001@gmail.com>
2023-04-13 01:41:27 +0200

    add average reaction in search and relative style

--- template/search.php
+++ template/search.php
@@ -5,15 +5,42 @@
 		<?php foreach ($usersList as $user) : ?>
 			<?php if ($user["Username"] !== $_SESSION["loggedUser"]) : ?>
 				<div class="card p-2">
-					<div class="">
+					<div class="user-info">
 						<img src="<?php echo UPLOAD_DIR . $user["FileName"] ?>" alt="profile-picture" class="profile-pic" />
-						<div class="">
-							<h2 class="fw-bold">
-								<a href="user.php?username=<?php echo $user["Username"] ?>">
-									@<?php echo $user["Username"] ?>
-								</a>
-								<br>
-							</h2>
+						<div>
+							<div class="user">
+								<h2 class="fw-bold">
+									<a href="user.php?username=<?php echo $user["Username"] ?>">
+										@<?php echo $user["Username"] ?>
+									</a>
+									<br>
+								</h2>
+								<?php
+								switch ($mysqli->users()->getAverageReactionByUsername($user["Username"])) {
+									case 1:
+										$reaction = "../public/assets/img/reaction-1.png";
+										break;
+									case 2:
+										$reaction = "../public/assets/img/reaction-2.png";
+										break;
+									case 3:
+										$reaction = "../public/assets/img/reaction-3.png";
+										break;
+									case 4:
+										$reaction = "../public/assets/img/reaction-4.png";
+										break;
+									case 5:
+										$reaction = "../public/assets/img/reaction-5.png";
+										break;
+									default:
+										$reaction = "";
+										break;
+								}
+								if ($reaction != "") : ?>
+									<img src="<?php echo $reaction; ?>" alt="average reaction" class="average-reaction">
+								<?php endif;
+								?>
+							</div>
 							<p class="text-muted"><?php echo $user["Bio"] ?></p>
 						</div>
 					</div>
