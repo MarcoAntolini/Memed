808da859cb0145f49f2fdf40010b027167978acf
Marco Antolini <marco.antolini2001@gmail.com>
2023-04-14 00:05:49 +0200

    format code

--- template/notification.php
+++ template/notification.php
@@ -1,14 +1,14 @@
-<section class="notification-container di-flex p-2 card"">
-    <div class=" row">
-	<form action="#" method="post">
-		<div>
-			<p class="fw-bold">Hai <span id="unread-notification-number"></span> notifiche non lette</p>
-			<button id="readall" class="btn btn-success" type="submit" name="readAll">Leggi tutto</button>
-			<button id="clearall" class="btn btn-danger" type="submit" name="deleteAll">Cancella tutto</button>
-		</div>
-	</form>
-	<?php if (isset($templateParams["js"])) : ?>
-		<div id="notification-section"></div>
-	<?php endif ?>
+<section class="notification-container di-flex p-2 card">
+	<div class=" row">
+		<form action="#" method="post">
+			<div>
+				<p class="fw-bold">Hai <span id="unread-notification-number"></span> notifiche non lette</p>
+				<button id="readall" class="btn btn-success" type="submit" name="readAll">Leggi tutto</button>
+				<button id="clearall" class="btn btn-danger" type="submit" name="deleteAll">Cancella tutto</button>
+			</div>
+		</form>
+		<?php if (isset($templateParams["js"])) : ?>
+			<div id="notification-section"></div>
+		<?php endif ?>
 	</div>
 </section>
